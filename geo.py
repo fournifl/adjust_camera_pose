6fc9406fddb169db83e62d9adc82cc8dfb37ee69
Florent <florent.fournier@wavesnsee.com>
2025-09-04 13:21:20 +0200

    gather some functions in scripts 'geo' and 'read_inputs'

---
+++ geo.py
@@ -0,0 +1,15 @@
+def pix_2_world(uvz, georef_params):
+    # convert pix points to geo local
+    geo_local = georef_params.pix2geo(uvz)
+    geo_world = georef_params.local_srs.m_w_l @ geo_local
+    return geo_world
+
+def world_2_pix(xyz, georef_params):
+    geo_local = georef_params.local_srs.m_l_w @ xyz
+    u, v = georef_params.geo2pix(geo_local)
+    return u ,v
+
+def compute_xyz_from_pix_and_uv_from_geo(uvz, xyz, georef_params):
+    xyz_from_pix = pix_2_world(uvz, georef_params)
+    u, v = world_2_pix(xyz, georef_params)
+    return xyz_from_pix, u, v
---
+++ read_inputs.py
@@ -0,0 +1,74 @@
+import cv2
+import pandas as pd
+import numpy as np
+from pyproj import Transformer
+import georaster
+import tifffile
+from geo import world_2_pix, pix_2_world, compute_xyz_from_pix_and_uv_from_geo
+
+def read_ortho(f_ortho):
+
+    # extent of tif file
+    band1 = georaster.SingleBandRaster(f_ortho, load_data=False)
+    extent = band1.extent # xmin, xmax, ymin, ymax
+
+    # get data
+    data = tifffile.imread(f_ortho)
+
+    return extent, data
+
+def read_img(f_img, georef_params):
+    img = cv2.imread(f_img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # undistort image
+    img = georef_params.undistort(img)
+    return img
+
+def read_tif_mnt(f_mnt, epsg_mnt, georef_params, ss_ech_factor=2):
+
+    # extent of tif file
+    band1 = georaster.SingleBandRaster(f_mnt, load_data=False)
+    xmin, xmax, ymin, ymax = band1.extent
+
+    # get mnt data
+    data = tifffile.imread(f_mnt)
+
+    # create masked array
+    data = np.ma.array(data, mask=data < -32760)
+
+    # get x, y grid coordinates
+    x = np.linspace(xmin, xmax, data.shape[1])
+    y = np.linspace(ymax, ymin, data.shape[0])
+    x, y = np.meshgrid(x, y)
+    x = x[~data.mask].flatten()
+    y = y[~data.mask].flatten()
+    transformer = Transformer.from_crs(epsg_mnt, georef_params.georef_local_srs.horizontal_srs.srid, always_xy=True)
+    x, y = transformer.transform(x, y)
+
+    # compress data
+    data = data.compressed().flatten()
+
+    # mnt uv from geo
+    u, v = world_2_pix(np.vstack([x, y, data]), georef_params)
+
+    return data[::ss_ech_factor], x[::ss_ech_factor], y[::ss_ech_factor], u[::ss_ech_factor], v[::ss_ech_factor]
+
+def read_remarkable_pts(f_corresp_pts_remarquables, georef_params):
+    df_corresp_pts_remarquables = pd.read_csv(f_corresp_pts_remarquables,
+                                              usecols=['easting', 'northing', 'elevation', 'U', 'V'])
+    # create U_undist and V_undist
+    undist_pts = cv2.undistortPoints(np.array(df_corresp_pts_remarquables[['U', 'V']]).astype(float),
+                                     georef_params.intrinsic_parameters.camera_matrix,
+                                     georef_params.distortion_coefficients.array,
+                                     P=georef_params.intrinsic_parameters.camera_matrix)
+    undist_pts = undist_pts.reshape((undist_pts.shape[0], undist_pts.shape[2]))
+    df_corresp_pts_remarquables['U_undist'] = undist_pts[:, 0]
+    df_corresp_pts_remarquables['V_undist'] = undist_pts[:, 1]
+
+    # remarkable pts xyz from pix, and uv from geo
+    xyz_remarkables_from_pix, u_remarkables_from_geo, v_remarkables_from_geo = (
+        compute_xyz_from_pix_and_uv_from_geo(df_corresp_pts_remarquables[['U', 'V', 'elevation']],
+                                             np.array(
+                                                 df_corresp_pts_remarquables[['easting', 'northing', 'elevation']]),
+                                             georef_params))
+    return df_corresp_pts_remarquables, xyz_remarkables_from_pix, u_remarkables_from_geo, v_remarkables_from_geo
